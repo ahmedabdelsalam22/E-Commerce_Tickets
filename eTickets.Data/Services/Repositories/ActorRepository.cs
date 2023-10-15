252944b48640ebb3c62a794f520f29715a24201d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-15 14:30:23 +0300

    create IActorRepository and ActorRepository and add DI in program.cs

---
+++ eTickets.Data/Services/Repositories/ActorRepository.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data.Services.IRepositories;
+using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.Repositories
+{
+    public class ActorRepository : GenericRepository<Actor>, IActorRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ActorRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task Update(Actor actor)
+        {
+            _context.Update(actor);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
