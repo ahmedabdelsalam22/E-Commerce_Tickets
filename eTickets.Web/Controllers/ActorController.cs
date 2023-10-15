5bf3d21657fdbfb8511412d8f87319459b5f1e77
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-15 14:53:28 +0300

    build index ui to display all actors

--- eTickets.Web/Controllers/ActorController.cs
+++ eTickets.Web/Controllers/ActorController.cs
@@ -1,5 +1,7 @@
-﻿using eTickets.Data.Services.UnitOfWork;
+﻿using AutoMapper;
+using eTickets.Data.Services.UnitOfWork;
 using eTickets.Models;
+using eTickets.Models.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eTickets.Web.Controllers
@@ -7,17 +9,21 @@ namespace eTickets.Web.Controllers
     public class ActorController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public ActorController(IUnitOfWork unitOfWork)
+        public ActorController(IUnitOfWork unitOfWork,IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
             IEnumerable<Actor> actors = await _unitOfWork.actorRepository.GetAllAsync();
 
-            return View();
+            List<ActorDto> actorDtos = _mapper.Map<List<ActorDto>>(actors);
+
+            return View(actorDtos);
         }
     }
 }
