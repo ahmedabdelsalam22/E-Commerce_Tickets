5fed079ab92f9345039847c6efb3d90dde25ddba
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-15 14:24:59 +0300

    create IGenericRepository and GenericRepository

---
+++ eTickets.Data/Services/IRepositories/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.IRepositories
+{
+    public interface IGenericRepository<T> where T:class
+    {
+        Task<IEnumerable<T>> GetAllAsync(bool tracked = true);
+        Task<T> GetAsync(Expression<Func<T,bool>>? filter=null , bool tracked=true);
+        Task Create(T entity);
+        Task Delete(T entity);
+    }
+}
