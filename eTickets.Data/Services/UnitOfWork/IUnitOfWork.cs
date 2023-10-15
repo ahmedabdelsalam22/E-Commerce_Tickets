01c18d672ad306960cf7a368c0a59b299dbde688
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-15 14:36:32 +0300

    create IUnitOfWork and UnitOfWork and register DI in program.cs

---
+++ eTickets.Data/Services/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using eTickets.Data.Services.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IActorRepository actorRepository { get; }
+    }
+}
---
+++ eTickets.Data/Services/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using eTickets.Data.Services.IRepositories;
+using eTickets.Data.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            actorRepository = new ActorRepository(_context);
+        }
+        public IActorRepository actorRepository { get; private set; }
+    }
+}
