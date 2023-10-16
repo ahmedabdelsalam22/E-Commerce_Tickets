1e24b26afd1e3403c3370da1da11274d31e6a1b5
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-16 15:36:34 +0300

    create ICinemaRepository and CinemaRepository

---
+++ eTickets.Data/Services/IRepositories/ICinemaRepository.cs
@@ -0,0 +1,15 @@
+﻿using eTickets.Data.Services.Repositories;
+using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.IRepositories
+{
+    public interface ICinemaRepository : IGenericRepository<Cinema>
+    {
+        Task Update(Cinema cinema);
+    }
+}
---
+++ eTickets.Data/Services/Repositories/CinemaRepository.cs
@@ -0,0 +1,25 @@
+﻿using eTickets.Data.Services.IRepositories;
+using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.Repositories
+{
+    public class CinemaRepository : GenericRepository<Cinema>, ICinemaRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CinemaRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task Update(Cinema cinema)
+        {
+            _context.Update(cinema);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
