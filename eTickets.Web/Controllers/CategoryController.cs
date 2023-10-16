833d9c56f8bf929a8959dc1a29a42f6f5500fe0d
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-16 20:13:04 +0300

    create CategoryController and getting all categories

---
+++ eTickets.Web/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using eTickets.Data.Services.UnitOfWork;
+using eTickets.Models.Dtos;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CategoryController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Category> categories = await _unitOfWork.categoryRepository.GetAllAsync(tracked: false);
+
+            List<CategoryDto> categoryDtos = _mapper.Map<List<CategoryDto>>(categories);
+
+            return View(categoryDtos);
+        }
+
+    }
+}
