1ed2dff667b69a941b9d405833937aa708b0f9a8
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-18 19:32:18 +0300

    create ShoppingCart class to get  GetShoppingCartItems and get GetShoppingCartTotal

--- eTickets.Data/ApplicationDbContext.cs
+++ eTickets.Data/ApplicationDbContext.cs
@@ -22,6 +22,7 @@ namespace eTickets.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
 
