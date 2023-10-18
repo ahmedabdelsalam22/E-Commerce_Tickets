432f5fbf6e4dcece782762cb7e82fba63910327b
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-10-18 20:22:05 +0300

    refactor ShoppingClass logic to IShoppingCart and ShoppingCart

---
+++ eTickets.Data/Services/IRepositories/IShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.IRepositories
+{
+    public interface IShoppingCart
+    {
+        Task<List<ShoppingCartItem>> GetShoppingCartItems();
+        double GetShoppingCartTotal();
+        Task AddItemToCart(Movie movie);
+        Task RemoveItemFromCart(Movie movie);
+    }
+}
