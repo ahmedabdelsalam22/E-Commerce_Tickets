e8ce7827718ce321397a92948fdc2d580fb55564
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-11-22 23:16:16 +0200

    create IOrderService

---
+++ eTickets.Data/Services/IRepositories/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services.IRepositories
+{
+    public interface IOrderService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+        Task<List<Order>> GetOrdersByUserIdAndRoleAsync(string userId, string userRole);
+    }
+}
